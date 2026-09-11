fbfbd0a73596d30c33cf76684c5e44cdd5822fbf
Kd-26 <coderkd26@gmail.com>
2026-09-11 21:01:13 +0530

    feat: add packages/capsule/src/llmopt_capsule/__init__.py

---
+++ packages/capsule/src/llmopt_capsule/__init__.py
@@ -0,0 +1,7 @@
+"""Execution Capsule metadata only."""
+
+from llmopt_capsule.manifest import ArtifactDescriptor, ExecutionCapsuleManifest, IntegrityMetadata
+
+__all__ = ["ArtifactDescriptor", "ExecutionCapsuleManifest", "IntegrityMetadata"]
+
+__version__ = "0.1.0.dev0"
