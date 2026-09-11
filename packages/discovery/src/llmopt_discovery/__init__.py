94e8b043a91e6ad7ec3f4c60e13373e93e16f304
Kd-26 <coderkd26@gmail.com>
2026-09-11 22:07:00 +0530

    feat: add packages/discovery/src/llmopt_discovery/__init__.py

---
+++ packages/discovery/src/llmopt_discovery/__init__.py
@@ -0,0 +1,7 @@
+"""Infrastructure and model discovery ports."""
+
+from llmopt_discovery.interfaces import InfrastructureInspector, ModelInspector
+
+__all__ = ["InfrastructureInspector", "ModelInspector"]
+
+__version__ = "0.1.0.dev0"
