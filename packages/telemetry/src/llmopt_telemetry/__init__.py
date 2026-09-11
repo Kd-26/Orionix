9b66e1fbcdf1e75b2e24810504796d5059374fa9
Kd-26 <coderkd26@gmail.com>
2026-09-11 21:01:14 +0530

    feat: add packages/telemetry/src/llmopt_telemetry/__init__.py

---
+++ packages/telemetry/src/llmopt_telemetry/__init__.py
@@ -0,0 +1,7 @@
+"""No-op-by-default telemetry interfaces."""
+
+from llmopt_telemetry.interfaces import Metric, MetricCategory, MetricsSink, NoOpMetricsSink
+
+__all__ = ["Metric", "MetricCategory", "MetricsSink", "NoOpMetricsSink"]
+
+__version__ = "0.1.0.dev0"
