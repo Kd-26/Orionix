96c06843f1f6e37fdae965f7fcad57661af31d8a
Kd-26 <coderkd26@gmail.com>
2026-09-11 22:06:58 +0530

    feat: update packages/integrations/src/llmopt_integrations/__init__.py

--- packages/integrations/src/llmopt_integrations/__init__.py
+++ packages/integrations/src/llmopt_integrations/__init__.py
@@ -1,3 +1,7 @@
-"""Vendor integration namespaces; no adapters are implemented."""
+"""Provider-neutral ports and vendor adapter namespaces."""
+
+from llmopt_integrations.ports import EngineAdapter, ExecutionBackend, ProvisioningBackend
+
+__all__ = ["EngineAdapter", "ExecutionBackend", "ProvisioningBackend"]
 
 __version__ = "0.1.0.dev0"
